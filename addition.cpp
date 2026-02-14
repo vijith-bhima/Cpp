9b0a8d4a1915d377f5921ae4ad56e1446032121a
Vijith <vijith0999@gmail.com>
2026-02-14 23:07:42 +0530

    addition

---
+++ addition.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n1;
+  int n2;
+  int c;
+  cout<<"enter n1 value"<<endl;
+  cin>>n1;
+  cout<<"enter n2 value"<<endl;
+  cin>>n2;
+   c= n1+n2;
+  cout<<"sum of two numbers is "<<c<<endl;
+  return 0;
+}
