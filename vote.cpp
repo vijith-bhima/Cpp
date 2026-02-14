9ca5381f44934197b5e3b3a5621ce8ec0c3c34e1
Vijith <vijith0999@gmail.com>
2026-02-14 23:25:17 +0530

    vote

---
+++ vote.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int age;
+    cout<<"enter your age"<<endl;
+    cin>>age;
+    if(age>=18){
+        cout<<"your eligible to vote"<<endl;
+    }
+    else {
+        cout<<"you are not eligible to vote"<<endl;
+
+    }
+    return 0;
+    }
+    
