9b3a60b865b1cf0b430a01f6c81cc37cbc19ea8b
Vijith <vijith0999@gmail.com>
2026-02-26 18:29:33 +0530

    areaofcircle

---
+++ area_of_circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+const float PI = 3.14;
+int main(){
+    float r,area;
+    cout<<"enter the radius of circle"<<endl;
+    cin>>r;
+    area = PI * r*r;
+    cout<<"radius of circle is "<<ends<<area<<endl;
+    return 0;
+}
