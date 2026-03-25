e264580228dd0ab9708c38f20dc08be6e01d7203
Vijith <vijith0999@gmail.com>
2026-03-25 18:59:55 +0530

    area of triangle

---
+++ area_of_triangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+  int height, base;
+   cout<<"enter height and base";
+   cin>>height>>base;
+   int area;
+       area = 0.5*height*base;
+       cout<<"area of triangle is "<<area;
+       return 0;
+}
